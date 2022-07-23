7f220e156e5ab2139e7dd162ac4e4b26219b57ab
Anthony O'Connor <anthony@modernadvisor.ca>
2022-07-23 10:12:53 -0700

    Attempt caching to allow offline working

---
+++ sw.js
@@ -0,0 +1,61 @@
+self.addEventListener('install', (event) => {
+    console.log(event);
+});
+
+self.addEventListener('activate', (event) => {
+    console.log(event);
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log(event);
+});
+
+const assetsCacheName = 'spelling-quest-assets';
+self.addEventListener('fetch', (event) => {
+    // Ignore crossdomain requests
+    if (!event.request.url.startsWith(self.location.origin)) {
+        return;
+    }
+    // Ignore non-GET requests
+    if (event.request.method !== 'GET') {
+        return;
+    }
+    // Ignore browser-sync
+    if (event.request.url.indexOf('browser-sync') > -1) {
+        return;
+    }
+    // Prevent index route being cached
+    if (event.request.url === (self.location.origin + '/')) {
+        return;
+    }
+    // Prevent index.html being cached
+    if (event.request.url.endsWith('index.html')) {
+        return;
+    }
+    console.log(event);
+    // Tell the fetch to respond with this chain
+    event.respondWith(
+        // Open the cache
+        caches.open(assetsCacheName)
+            .then((cache) => {
+                // Look for matching request in the cache
+                return cache.match(event.request)
+                    .then((matched) => {
+                        // If a match is found return the cached version first
+                        if (matched) {
+                            console.log("matched version");
+                            return matched;
+                        }
+                        // Otherwise continue to the network
+                        return fetch(event.request)
+                            .then((response) => {
+                                // Cache the response
+                                console.log("saving cached");
+                                cache.put(event.request, response.clone());
+                                // Return the original response to the page
+                                return response;
+                            });
+                    });
+            })
+    );
+});
